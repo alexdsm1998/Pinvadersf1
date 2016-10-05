50e7a8aca89210569a48d0eccbd8b403e870918d
Alejandro Del sanz Masia <aldema@floridauniversitaria.es>
2016-10-05 12:46:42 +0200

    first commit

---
+++ ia.php
@@ -0,0 +1,19 @@
+<?php
+
+class ia
+{
+
+//celdas y columnas
+public $columna= 2 ;
+public $fila= 3 ;
+
+//método
+public function getColumna() {
+ return $this->columna;
+}
+public function getfila() {
+ return $this->fila;
+ }
+}
+
+?>
